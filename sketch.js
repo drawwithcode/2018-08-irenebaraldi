149661df7307e90fc97c7edb4f126e594771fa03
Irene Baraldi <ire.baraldi@hotmail.it>
2018-11-20 23:19:43 +0100

    es08-finito

--- sketch.js
+++ sketch.js
@@ -1,11 +1,48 @@
-function preload(){
-  // put preload code here
-}
+var value = 0;
+var r = 0;
+var g = 0;
+var b = 0;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+
+  angleMode(DEGREES);
+
+  // by defaults equals to 30
+  setShakeThreshold(10);
+  frameRate(12);
 }
 
 function draw() {
-  // put drawing code here
+  background(r, g, b);
+
+
+    fill(g, b, r);
+    noStroke();
+    ellipse(width/2, height/2, value*1.1);
+
+fill(255);
+text('shake and rotate', 100, 100);
+textSize(100);
+
+}
+
+function deviceTurned(){
+   myTurnedAxis = turnAxis;
+
+   if(turnAxis === 'Z') {
+		b = random()*255;
+	} else if(turnAxis === 'X') {
+		r = random()*255;
+	} else if(turnAxis === 'Y') {
+		g = random()*255;
+	}
+
+}
+
+function deviceShaken() {
+  value = value + 1;
+  if (value > 255) {
+    value = 0;
+  }
 }
